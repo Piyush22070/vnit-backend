b8e7c286e1cb37ce9d0357eb94e4a5c6ecbdd848
dsmundhe <dipakmundhe2026@gmail.com>
2025-03-23 00:49:17 -0700

    Initial commit for dipak branch

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['lh3.googleusercontent.com'], // Add the external domain here
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ["lh3.googleusercontent.com", "i.pinimg.com"], // Add allowed domains
+  },
+};
+
+export default nextConfig;
